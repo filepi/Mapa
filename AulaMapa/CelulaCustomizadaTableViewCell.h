e4dd8128a3e87826d8c67cef16ac06bc67944733
Treinamento <treinamento@Treinamento1-Maquina17.local>
2017-08-19 11:51:39 -0300

    PESQUISA LOCATION

---
+++ AulaMapa/CelulaCustomizadaTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  CelulaCustomizadaTableViewCell.h
+//  AulaMapa
+//
+//  Created by Treinamento on 19/08/17.
+//  Copyright © 2017 Treinamento. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CelulaCustomizadaTableViewCell : UITableViewCell
+
+@end
---
+++ AulaMapa/CelulaCustomizadaTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  CelulaCustomizadaTableViewCell.m
+//  AulaMapa
+//
+//  Created by Treinamento on 19/08/17.
+//  Copyright © 2017 Treinamento. All rights reserved.
+//
+
+#import "CelulaCustomizadaTableViewCell.h"
+
+@implementation CelulaCustomizadaTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
